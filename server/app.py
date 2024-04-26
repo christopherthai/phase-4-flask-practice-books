a1438a52b8c56bf5817d37ce3b8fc8bc37557512
Christopher Thai <christopherthai.nyli@gmail.com>
2024-04-26 13:02:28 -0400

    Complete Lab

--- server/app.py
+++ server/app.py
@@ -4,11 +4,11 @@ from flask import Flask, make_response, jsonify, request
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 
-from models import db # import your models here!
+from models import db, Author, Publisher, Book
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -17,11 +17,101 @@ db.init_app(app)
 
 api = Api(app)
 
-@app.get('/')
+
+@app.get("/")
 def index():
     return "Hello world"
 
-# write your routes here!
 
-if __name__ == '__main__':
+# write your routes here!
+class AuthorsByID(Resource):
+
+    def get(self, id):
+        author = Author.query.get(id)
+
+        if author is None:
+            make_response({"error": "Author not found"}, 404)
+        else:
+            return make_response(
+                {
+                    "id": author.id,
+                    "name": author.name,
+                    "pen_name": author.pen_name,
+                    "books": [book.to_dict() for book in author.books],
+                },
+                200,
+            )
+
+    def delete(self, id):
+
+        author = Author.query.get(id)
+
+        if author is None:
+            return make_response({"error": "Author not found"}, 404)
+        else:
+            db.session.delete(author)
+            db.session.commit()
+            return make_response({"message": "Author deleted"}, 200)
+
+
+api.add_resource(AuthorsByID, "/authors/<int:id>")
+
+
+class Books(Resource):
+    def get(self):
+
+        try:
+            books = [book.to_dict() for book in Book.query.all()]
+            return make_response(books, 200)
+        except Exception:
+            return make_response({"error": "Books not found"}, 404)
+
+    def post(self):
+
+        try:
+            data = request.json
+
+            new_book = Book(
+                title=data["title"],
+                page_count=data["page_count"],
+                author_id=data["author_id"],
+                publisher_id=data["publisher_id"],
+            )
+
+            db.session.add(new_book)
+            db.session.commit()
+
+            return make_response(new_book.to_dict(), 201)
+
+        except Exception:
+            return make_response({"error": "Invalid data"}, 400)
+
+
+api.add_resource(Books, "/books")
+
+
+class PublisherByID(Resource):
+
+    def get(self, id):
+
+        publisher = Publisher.query.get(id)
+
+        if publisher is None:
+            return make_response({"error": "Publisher not found"}, 404)
+        else:
+            return make_response(
+                {
+                    "id": publisher.id,
+                    "name": publisher.name,
+                    "founding_year": publisher.founding_year,
+                    "authors": [
+                        book.author.to_dict(rules=("-books",))
+                        for book in publisher.books
+                    ],
+                },
+                200,
+            )
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,10 +4,77 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 # write your models here!
+class Author(db.Model, SerializerMixin):
+    __tablename__ = "authors"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    pen_name = db.Column(db.String)
+
+    books = db.relationship("Book", back_populates="author", cascade="all, delete")
+    publishers = association_proxy("book", "publisher")
+
+    serialize_rules = ("-books.author", "-publishers.authors")
+
+    def __repr__(self):
+        return f"<Author id={self.id} name={self.name} pen_name={self.pen_name} />"
+
+
+class Publisher(db.Model, SerializerMixin):
+    __tablename__ = "publishers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    founding_year = db.Column(db.Integer, nullable=False)
+
+    serialize_rules = "-books.publisher"
+
+    books = db.relationship("Book", back_populates="publisher")
+    authors = association_proxy("books", "author")
+
+    @validates("founding_year")
+    def validates_year(self, key, value):
+        if 1600 <= value <= 2024:
+            return value
+        else:
+            raise ValueError("1600 <= value <= 2024")
+
+    def __repr__(self):
+        return f"<Publisher id={self.id} name={self.name} founding_year={self.founding_year} />"
+
+
+class Book(db.Model, SerializerMixin):
+    __tablename__ = "books"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    page_count = db.Column(db.Integer)
+    author_id = db.Column(db.Integer, db.ForeignKey("authors.id"))
+    publisher_id = db.Column(db.Integer, db.ForeignKey("publishers.id"))
+
+    author = db.relationship("Author", back_populates="books", cascade="all, delete")
+    publisher = db.relationship(
+        "Publisher", cascade="all,delete", back_populates="books"
+    )
+
+    serialize_rules = ("-author.books", "-publisher.books")
+
+    @validates("page_count")
+    def validates_pages(self, key, value):
+        if value <= 0:
+            raise ValueError("Page must be greater than 0")
+        else:
+            return value
+
+    def __repr__(self):
+        return f"<Book id={self.id} title={self.title} page_count={self.page_count} />"
